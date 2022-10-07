f83bb318c30a9b1c5edbfc0c3045a17c322a8e6d
michel benjamin <mcb4419a@ens.fsi>
2022-10-07 09:35:26 +0200

    fin séance 2

---
+++ src/personnages/Druide.java
@@ -0,0 +1,47 @@
+package personnages;
+import java.util.Random;
+
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	private int forcePotion = 1;
+	
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une focre " + effetPotionMin + " à "
+				+ "" + effetPotionMax + ".");
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+	public String prendreParole() {
+		return "Le druide" + nom + " : ";
+	}
+	
+	public void preparerPotion(Druide druide) {
+		Random random = new Random();
+		while (forcePotion < 5) {
+			forcePotion = random.nextInt(effetPotionMax);
+		}
+		if (forcePotion > 7) { 
+			druide.parler("J'ai préparé une super potion de force "+forcePotion);
+		} else { 
+			druide.parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force "+forcePotion);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion(panoramix);
+	}
+
+}
---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + " »");
+	}
+
+	public String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup((force / 3)*effetPotion);
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public void boirePotion(Gaulois gaulois, int forcePotion) {
+		effetPotion = forcePotion;
+		gaulois.parler(" Merci Druide, je sens que ma force est "+ forcePotion +" fois décuplée.");
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 4);
+		Romain cesar = new Romain("César", 1);
+//		System.out.println(asterix.nom);
+//		System.out.println(asterix);
+//		System.out.println(asterix.prendreParole());
+//		asterix.parler("je parle.");
+//		System.out.println(cesar.prendreParole());
+//		asterix.frapper(cesar);
+		asterix.boirePotion(asterix,8);
+	}
+
+}
